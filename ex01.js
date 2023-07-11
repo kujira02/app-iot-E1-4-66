d61cbf46e7e25a159f9c8ebd38d4ddb5e5972552
kujira02 <zomo00235@gmail.com>
2023-07-11 13:57:42 +0700

    first

--- ex01.js
+++ ex01.js
@@ -8,9 +8,10 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! naphat')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+  console.log(`bruh`)
 })
